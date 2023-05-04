d7f96c242eb93ea7c401e47108c53dda3a357e69
wtrewa <psaurabh574@gmail.com>
2023-05-04 22:39:43 +0530

    adminday2

---
+++ clothy/src/Redux/Admin/reducer.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+    isLoading:false,
+    isErr:false,
+    product:[]
+}
+
+export const reducer = (state=initialState, {type,payload})=>{
+    switch (type) {
+       
+            
+    
+        default:
+            return state;
+    }
+}
--- clothy/src/Routes/AllRoutes.jsx
+++ clothy/src/Routes/AllRoutes.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../Pages/HomePage";
+import AdminPage from "../Pages/AdminPage";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path={"/"} element={<HomePage />} />
+      <Route path="/admin" element={<AdminPage/>}/>
+      <Route path="*" element={<h1>404 Page Not Found</h1>}/>
     </Routes>
   );
 };
