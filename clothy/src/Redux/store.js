06360e450f7369b6c62bffc27115c35a8512d6e3
Surajy001 <soorajy001@gmail.com>
2023-05-05 23:57:05 +0530

    Day-3 signin and signup

--- clothy/src/Redux/store.js
+++ clothy/src/Redux/store.js
@@ -1,6 +1,9 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
+import {authReducer} from "./AuthReducer/reducer";
 
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({
+    authReducer,
+ });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
